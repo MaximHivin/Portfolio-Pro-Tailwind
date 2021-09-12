1a0f4e35104f5b82bffcbec2edbe683d952fa29f
Maxime Hivin <maximehivin@MacBook-Air-de-Maxime.local>
2021-09-12 19:08:55 +0200

    first portfolio

---
+++ app.js
@@ -0,0 +1,16 @@
+const menuResponsive = document.querySelector('.toggle-menu');
+const iconeToggle = document.querySelector('.icone-toggle');
+
+
+iconeToggle.addEventListener('click', () => {
+
+    menuResponsive.classList.toggle('hidden');
+    
+    
+});
+
+
+
+
+
+
