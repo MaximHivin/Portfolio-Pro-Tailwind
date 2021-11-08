b61e976f92f3ce53caae6346a0574ca4d1147c60
MaximHivin <hivin.maxim@hotmail.fr>
2021-11-08 10:47:06 +0100

    ajout partie Blog

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+// On selectionne le menu à cacher
 const menuResponsive = document.querySelector('.toggle-menu');
+
+// On selectionne le bouton pour cacher le menu
 const iconeToggle = document.querySelector('.icone-toggle');
 
+// On déclare un événement au 'click' en utilisant notre fonction 'handleclick'
+iconeToggle.addEventListener('click', handleclick);
 
-iconeToggle.addEventListener('click', () => {
+// On crée la fonction 'handleclick'
+function handleclick() {
 
     menuResponsive.classList.toggle('hidden');
-    
-    
-});
+
+};
 
 
 
